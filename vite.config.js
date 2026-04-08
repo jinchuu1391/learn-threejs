0c792339cb599246d8d6a8a2c44cf26f6014e099
jinchuu1391 <jinchuu1391@gmail.com>
2026-04-08 15:20:32 +0900

    WebGL Fundamentals 예제

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
   root: "src",
@@ -7,6 +11,12 @@ export default defineConfig({
   build: {
     outDir: "../dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        webglFundamentals: resolve(__dirname, "src/webgl-fundamentals.html"),
+      },
+    },
   },
   plugins: [glsl()],
   base: "/learn-threejs/",
